736a31dc040248d5f858c27dbfcc65f3701d7402
ARTURO TELLEZ <artec_94@hotmail.com>
2021-10-20 15:57:55 -0500

    correcciones y codigo nuevo

---
+++ RScripts/Actualizar_R.R
@@ -0,0 +1,7 @@
+## Actualizar R
+#https://www.linkedin.com/pulse/3-methods-update-r-rstudio-windows-mac-woratana-ngarmtrakulchol/
+install.packages("installr")
+
+library(installr)
+
+updateR()
---
+++ RScripts/Escribir_excel.R
@@ -0,0 +1,16 @@
+
+rm(list = ls())
+
+#importar paquetes
+library(datasets)
+library(readr) # https://readr.tidyverse.org/reference/write_delim.html
+library(openxlsx) # Escribir excel
+write.xlsx(datasets::Titanic, "data.xlsx")
+
+readr::write_xlsx
+#Limpieza de nuestra sesión
+
+#Ver datasets
+class(datasets::Titanic)
+
+
